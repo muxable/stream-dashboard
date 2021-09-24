7df007b9ce1a50a611fefa34f8cb3e8728a0f64f
ChuangWei Ma <chungwwei223@gmail.com>
2021-09-24 15:11:52 -0400

    chore: tab spacing format

--- src/component/SignUp.tsx
+++ src/component/SignUp.tsx
@@ -1,51 +1,51 @@
 import { Container, Box, Button, Grid, TextField } from "@mui/material";
 
 export function SignUp() {
-	return (
-		<Container>
-			<p> Stream Dashboard by Muxable</p>
-			<Grid container spacing={3} direction='column' alignContent='center'>
-				<Grid item>
-					<Box width={350}>
-						<TextField
-							fullWidth
-							label="Email"
-							name="email"
-							size="small"
-							variant="standard"
-						/>
-					</Box>
-				</Grid>
-				<Grid item>
-					<Box width={350}>
-						<TextField
-							fullWidth
-							label="Password"
-							name="password"
-							size="small"
-							type="password"
-							variant="standard"
-						/>
-					</Box>
-				</Grid>
-				<Grid item>
-					<Box width={350}>
-						<TextField
-							fullWidth
-							label="Confirm password"
-							name="Confirm password"
-							size="small"
-							type="password"
-							variant="standard"
-						/>
-					</Box>
-				</Grid>
-				<Grid item>
-					<Box width={350}>
-						<Button fullWidth size='medium' variant="outlined"> Create Account </Button>
-					</Box>
-				</Grid>
-			</Grid>
-		</Container >
-	)
+  return (
+    <Container>
+      <p> Stream Dashboard by Muxable </p>
+      <Grid container spacing={3} direction='column' alignContent='center'>
+        <Grid item>
+          <Box width={350}>
+            <TextField
+              fullWidth
+              label="Email"
+              name="email"
+              size="small"
+              variant="standard"
+            />
+          </Box>
+        </Grid>
+        <Grid item>
+          <Box width={350}>
+            <TextField
+              fullWidth
+              label="Password"
+              name="password"
+              size="small"
+              type="password"
+              variant="standard"
+            />
+          </Box>
+        </Grid>
+        <Grid item>
+          <Box width={350}>
+            <TextField
+              fullWidth
+              label="Confirm password"
+              name="Confirm password"
+              size="small"
+              type="password"
+              variant="standard"
+            />
+          </Box>
+        </Grid>
+        <Grid item>
+          <Box width={350}>
+            <Button fullWidth size='medium' variant="outlined"> Create Account </Button>
+          </Box>
+        </Grid>
+      </Grid>
+    </Container >
+  )
 }
