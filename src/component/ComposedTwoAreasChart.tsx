d9e62f1e36979bc45ebf5384f16d128468ba0f9b
ChuangWei Ma <chungwwei223@gmail.com>
2021-09-29 22:22:48 -0400

    feat: bitrate/fps biaxis chart and modem bandwidth chart

---
+++ src/component/ComposedTwoAreasChart.tsx
@@ -0,0 +1,56 @@
+import {
+	Brush,
+	ComposedChart,
+	Area,
+	XAxis,
+	YAxis,
+	Tooltip,
+	Legend
+} from "recharts";
+
+function getRandomArbitrary(min: number, max: number) {
+	return Math.ceil(Math.random() * (max - min) + min);
+}
+
+var data: any[] = [];
+for (let i = 0; i < 2000; i++) {
+	let d = {
+		upstream: getRandomArbitrary(0, 10),
+		downstream: getRandomArbitrary(0, 30)
+	};
+	data.push(d);
+}
+
+export default function ComposedTwoAreasChart() {
+	return (
+		<ComposedChart
+			width={600}
+			height={400}
+			data={data}
+			margin={{
+				top: 20,
+				right: 20,
+				bottom: 20,
+				left: 20
+			}}
+		>
+			<XAxis dataKey="name" tick={false} />
+			<YAxis label={{ value: "Mbps", angle: -90, position: "insideLeft" }} />
+			<Tooltip />
+			<Legend />
+			<Area
+				dot={false}
+				dataKey="downstream"
+				fill="#8884d8"
+				stroke="#8884d8"
+			/>
+			<Area
+				dot={false}
+				dataKey="upstream"
+				fill="#f5426c"
+				stroke="#f5426c"
+			/>
+			<Brush />
+		</ComposedChart>
+	);
+}
---
+++ src/component/ComposedTwoYAxisChart.tsx
@@ -0,0 +1,60 @@
+import {
+	Brush,
+	Line,
+	XAxis,
+	YAxis,
+	Tooltip,
+	Legend,
+	ComposedChart
+} from "recharts";
+
+function getRandomArbitrary(min: number, max: number) {
+	return Math.ceil(Math.random() * (max - min) + min);
+}
+
+var data: any[] = [];
+for (let i = 0; i < 2000; i++) {
+	let d = {
+		bitrate: getRandomArbitrary(3800, 4200),
+		framerate: getRandomArbitrary(59, 61)
+	};
+	data.push(d);
+}
+data[900]["bitrate"] = 2300;
+data[952]["bitrate"] = 2900;
+data[1000]["bitrate"] = 4500;
+data[1001]["bitrate"] = 4500;
+data[1022]["bitrate"] = 4300;
+data[1030]["bitrate"] = 4500;
+
+export default function App() {
+	return (
+		<ComposedChart
+			width={600}
+			height={400}
+			data={data}
+			margin={{
+				top: 20,
+				right: 20,
+				left: 20,
+				bottom: 20
+			}}
+		>
+			<XAxis dataKey="name" tick={false} />
+			<YAxis
+				yAxisId="left"
+				label={{ value: "Kbps", angle: -90, position: "insideLeft" }}
+			/>
+			<YAxis
+				yAxisId="right"
+				orientation="right"
+				label={{ value: "fps", angle: 90, position: "insideRight" }}
+			/>
+			<Tooltip />
+			<Legend />
+			<Line yAxisId="left" dot={false} dataKey="bitrate" stroke="#8884d8" />
+			<Line yAxisId="right" dot={false} dataKey="framerate" stroke="#82ca9d" />
+			<Brush />
+		</ComposedChart>
+	);
+}
