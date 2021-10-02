0245b9e4aa73fb97f4b84dde93ecf983cfad6838
ChuangWei Ma <chungwwei223@gmail.com>
2021-10-02 19:12:40 -0400

    chore: use type instead of class for metadata

--- src/component/StreamHealthTable.tsx
+++ src/component/StreamHealthTable.tsx
@@ -2,14 +2,9 @@ import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import { Chip, Container } from '@mui/material';
 
 
-export class HealthCellMetaData {
+type HealthCellMetaData = {
 	isStable: boolean;
 	duration: number;
-
-	constructor(isStable: boolean, duration: number) {
-		this.isStable = isStable;
-		this.duration = duration
-	}
 }
 
 const columns: GridColDef[] = [
@@ -38,28 +33,26 @@ const columns: GridColDef[] = [
 			const isStable = healthMetaData.isStable
 			const duration = healthMetaData.duration
 			return (
-				// < Chip label="stable" color="success" />
 				<Container>
 					{isStable && <Chip label={`stable(${duration}s)`} color="success" variant="outlined" />}
-					{!isStable && <Chip label={`unstable${duration}s`} color="error" variant="outlined" />}
+					{!isStable && <Chip label={`unstable(${duration})s`} color="error" variant="outlined" />}
 				</Container>
-
-				// { !health.isStable && < Chip label="unstable" color="success" /> }
 			);
 		}
 	},
 ];
 
+
 const rows = [
-	{ id: 1, timestamp: Date(), health: new HealthCellMetaData(true, 12) },
-	{ id: 2, timestamp: Date(), health: new HealthCellMetaData(false, 12) },
-	{ id: 3, timestamp: Date(), health: new HealthCellMetaData(false, 12) },
-	{ id: 4, timestamp: Date(), health: new HealthCellMetaData(true, 12) },
-	{ id: 5, timestamp: Date(), health: new HealthCellMetaData(true, 12) },
-	{ id: 6, timestamp: Date(), health: new HealthCellMetaData(true, 12) },
-	{ id: 7, timestamp: Date(), health: new HealthCellMetaData(false, 12) },
-	{ id: 8, timestamp: Date(), health: new HealthCellMetaData(false, 12) },
-	{ id: 9, timestamp: Date(), health: new HealthCellMetaData(true, 12) },
+	{ id: 1, timestamp: Date(), health: { isStable: true, duration: 12 } },
+	{ id: 2, timestamp: Date(), health: { isStable: false, duration: 32 } },
+	{ id: 3, timestamp: Date(), health: { isStable: false, duration: 22 } },
+	{ id: 4, timestamp: Date(), health: { isStable: true, duration: 12 } },
+	{ id: 5, timestamp: Date(), health: { isStable: false, duration: 42 } },
+	{ id: 6, timestamp: Date(), health: { isStable: true, duration: 12 } },
+	{ id: 7, timestamp: Date(), health: { isStable: true, duration: 12 } },
+	{ id: 8, timestamp: Date(), health: { isStable: false, duration: 52 } },
+	{ id: 9, timestamp: Date(), health: { isStable: true, duration: 12 } },
 ];
 
 export function StreamHealthTable() {
