35ae1ed1442b0fb722e23d9af361baca4d1e3af3
ChuangWei Ma <39935368+chungwwei@users.noreply.github.com>
2021-10-07 22:03:05 -0400

    feat: hardware info table (#38)
    * feat: hardware info table
    
    * chore: fix linting issue
    
    * chore: run prettier
    
    * chore: prettier try again
    
    * chore: wtf prettier
    
    * chore: prettier finally
    
    * chore: remove unnessary text tag

---
+++ src/component/HardwareInformationTable.tsx
@@ -0,0 +1,102 @@
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { Stack } from "@mui/material";
+
+type HardwareInformation = {
+  audioCodec: string;
+  videoCodec: string;
+  videoResolution: string;
+  avgBitrate: number;
+  avgFramerate: number;
+  avgAudioBitrate: number;
+  AVCLevel: number;
+  keyframeInterval: number;
+};
+
+function HardwareInformationStack({
+  title,
+  value,
+}: {
+  title: string;
+  value: string;
+}) {
+  return (
+    <Stack direction="column">
+      <h3> {title} </h3>
+      {value}
+    </Stack>
+  );
+}
+
+export function HardwareInformationTable({
+  hardwareInfo,
+}: {
+  hardwareInfo: HardwareInformation;
+}) {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 600 }}>
+        <TableBody>
+          <TableRow>
+            <TableCell align="center">
+              <HardwareInformationStack
+                title={"Audio Codec"}
+                value={hardwareInfo.audioCodec}
+              />
+            </TableCell>
+            <TableCell align="center">
+              <HardwareInformationStack
+                title={"Video Codec"}
+                value={hardwareInfo.videoCodec}
+              />
+            </TableCell>
+            <TableCell align="center">
+              <HardwareInformationStack
+                title={"Video Resolution"}
+                value={hardwareInfo.videoResolution}
+              />
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell align="center">
+              <HardwareInformationStack
+                title={"Avg Bitrate"}
+                value={`${hardwareInfo.avgBitrate}`}
+              />
+            </TableCell>
+            <TableCell align="center">
+              <HardwareInformationStack
+                title={"Avg Framerate"}
+                value={`${hardwareInfo.avgFramerate}`}
+              />
+            </TableCell>
+            <TableCell align="center">
+              <HardwareInformationStack
+                title={"Avg Audio Bitrate"}
+                value={`${hardwareInfo.avgAudioBitrate}`}
+              />
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell align="center">
+              <HardwareInformationStack
+                title={"AVC Level"}
+                value={`${hardwareInfo.AVCLevel}`}
+              />
+            </TableCell>
+            <TableCell align="center">
+              <HardwareInformationStack
+                title={"Keyframe Interval"}
+                value={`${hardwareInfo.keyframeInterval}`}
+              />
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
