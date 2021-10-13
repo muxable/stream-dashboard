f74ef71e3a3708d817ff2b5064ab5a3a931bcfb1
Juaoose <jjrg1994@gmail.com>
2021-10-13 18:50:42 -0500

    chore: remove AuthProvider since its in the child App

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
+import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import { AuthProvider } from "./context/AuthContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <AuthProvider>
       <App />
-    </AuthProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
