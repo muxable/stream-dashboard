a6048fdec53b903d34cec9fc02828a390c38c03e
Leeoku <leeoku@gmail.com>
2021-10-17 18:55:06 -0400

    feat: signin functionality

--- src/component/Login.tsx
+++ src/component/Login.tsx
@@ -1,4 +1,6 @@
+import React, { useState, useRef } from "react";
 import {
+  Alert,
   Box,
   Button,
   Container,
@@ -7,9 +9,10 @@ import {
   Typography,
 } from "@mui/material";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { auth } from "../firebaseSetup";
 import Logo from "./Logo";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 const provider = new GoogleAuthProvider();
 
@@ -18,11 +21,54 @@ export function Login() {
     signInWithPopup(auth, provider);
   }
 
+  const [error, setError] = useState<string>("");
+  const emailRef = useRef<HTMLInputElement | null>(null);
+  const passwordRef = useRef<HTMLInputElement | null>(null);
+  const history = useHistory();
+
+  const loginSubmit = async (event: any) => {
+    event.preventDefault();
+    setError("");
+    const loginAuth = getAuth();
+
+    if (passwordRef.current === null || emailRef.current === null) {
+      return;
+    }
+
+    if (!emailRef.current.value) {
+      setError("Email field is empty");
+      return;
+    }
+
+    if (!passwordRef.current.value) {
+      setError("Password field is empty");
+      return;
+    }
+
+    if (passwordRef.current.value.length < 6) {
+      setError("Password must be at least 6 characters");
+      return;
+    }
+
+    try {
+      await signInWithEmailAndPassword(
+        loginAuth,
+        emailRef.current.value,
+        passwordRef.current.value
+      );
+      history.push("/");
+    } catch (error) {
+      setError("Failed to login.");
+      console.error(error);
+    }
+  };
+
   return (
     <Container>
       <Logo />
       <p> Stream Dashboard by Muxable</p>
       <Grid container spacing={3} direction="column" alignContent="center">
+        {error && <Alert severity="error">{error}</Alert>}
         <Grid item>
           <Box width={350}>
             <TextField
@@ -31,6 +77,7 @@ export function Login() {
               name="email"
               size="small"
               variant="outlined"
+              inputRef={emailRef}
             />
           </Box>
         </Grid>
@@ -43,13 +90,18 @@ export function Login() {
               size="small"
               type="password"
               variant="outlined"
+              inputRef={passwordRef}
             />
           </Box>
         </Grid>
         <Grid item>
           <Box width={350}>
-            <Button fullWidth size="medium" variant="outlined">
-              {" "}
+            <Button
+              fullWidth
+              size="medium"
+              variant="outlined"
+              onClick={loginSubmit}
+            >
               Login{" "}
             </Button>
           </Box>
