9947da2cb3df973b2d26c13e119ab17b7ae50a73
Juan <juaoose@users.noreply.github.com>
2021-10-19 19:49:01 -0500

    feat: add google login on signup screen (#47)

--- src/component/SignUp.tsx
+++ src/component/SignUp.tsx
@@ -10,7 +10,14 @@ import {
 import { Link } from "react-router-dom";
 import Logo from "./Logo";
 import React, { useState, useRef } from "react";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
+
+const provider = new GoogleAuthProvider();
 
 export function SignUp() {
   const [error, setError] = useState<string>("");
@@ -56,6 +63,10 @@ export function SignUp() {
       console.error(error);
     }
   };
+
+  function signInWithGoogle() {
+    signInWithPopup(getAuth(), provider);
+  }
 
   return (
     <Container>
@@ -105,6 +116,18 @@ export function SignUp() {
           <Box width={350}>
             <Button fullWidth size="medium" variant="outlined" onClick={signup}>
               Create Account
+            </Button>
+          </Box>
+        </Grid>
+        <Grid item>
+          <Box width={350}>
+            <Button
+              fullWidth
+              size="medium"
+              variant="outlined"
+              onClick={signInWithGoogle}
+            >
+              Sign up with Google
             </Button>
           </Box>
         </Grid>
