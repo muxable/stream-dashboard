47bf99d06ead65cd4d73b85f20e56469c8b5f119
Leeoku <leeoku@gmail.com>
2021-11-28 17:35:48 -0500

    chore: add analytic to authrote

--- src/App.tsx
+++ src/App.tsx
@@ -3,47 +3,50 @@ import "./App.css";
 import { Login } from "./component/Login";
 import { SignUp } from "./component/SignUp";
 
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import { StreamsTableView } from "./component/TableView";
 import { Analytics } from "./screens/Analytics";
 import { AuthProvider, useAuthState } from "./context/AuthContext";
 
+const AuthenticatedRoute: React.FC<any> = ({ component: C, ...props }) => {
+  const { isAuthenticated } = useAuthState();
+  console.log("authstate", useAuthState());
+  console.log(`AuthenticatedRoute: ${isAuthenticated}`);
+  return (
+    <Route
+      {...props}
+      render={(routeProps) =>
+        isAuthenticated ? <C {...routeProps} /> : <Redirect to="/login" />
+      }
+    />
+  );
+};
 
-  const AuthenticatedRoute: React.FC<any> = ({ component:C, ...props }) => {
-    const { isAuthenticated } = useAuthState();
-    console.log('authstate', useAuthState())
-    console.log(`AuthenticatedRoute: ${isAuthenticated}`);
-    return (
-      <Route
-        {...props}
-        render={(routeProps) =>
-          isAuthenticated ? <C {...routeProps} /> : <Redirect to="/login" />
-        }
-      />
-    );
-  };
-
-  const UnauthenticatedRoute: React.FC<any> = ({ component: C, ...props }) => {
-    const { isAuthenticated } = useAuthState();
-    console.log(`UnauthenticatedRoute: ${isAuthenticated}`);
-    return (
-      <Route
-        {...props}
-        render={(routeProps) =>
-          !isAuthenticated ? <C {...routeProps} /> : <Redirect to="/" />
-        }
-      />
-    );
-  };
+const UnauthenticatedRoute: React.FC<any> = ({ component: C, ...props }) => {
+  const { isAuthenticated } = useAuthState();
+  console.log(`UnauthenticatedRoute: ${isAuthenticated}`);
+  return (
+    <Route
+      {...props}
+      render={(routeProps) =>
+        !isAuthenticated ? <C {...routeProps} /> : <Redirect to="/" />
+      }
+    />
+  );
+};
 function App() {
-
   return (
     <div className="App">
       <header className="App-header">
         <AuthProvider>
           <Router>
             <Switch>
-              <Route path="/signup">
+              {/* <Route path="/signup">
                 <SignUp />
               </Route>
               <Route path="/login">
@@ -55,14 +58,20 @@ function App() {
               <Route path="/">
                 <p>Your past streams</p>
                 <StreamsTableView userId="rippyae" />
-              </Route>
-              {/* <UnauthenticatedRoute exact path="/signup" component={SignUp} />
+              </Route> */}
+              <UnauthenticatedRoute exact path="/signup" component={SignUp} />
               <UnauthenticatedRoute exact path="/login" component={Login} />
               <AuthenticatedRoute
                 exact
                 path="/"
-                component={<StreamsTableView userId="rippyae" />}
-              /> */}
+                component={StreamsTableView}
+                userId="rippyae"
+              />
+              <AuthenticatedRoute
+                exact
+                path="/analytic/:streamId"
+                component={Analytics}
+              />
             </Switch>
           </Router>
         </AuthProvider>
