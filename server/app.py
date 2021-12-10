70feb921a083f69a8db6988c60fd014780299d4e
Kevin Wang <kevmo314@gmail.com>
2021-12-10 16:46:01 -0500

    feat: add productionizing dockerfile (#84)

--- server/app.py
+++ server/app.py
@@ -1,7 +1,10 @@
 from flask import Flask
 
-app = Flask(__name__)
+def create(test_source=None):
+    app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+    @app.route("/")
+    def hello_world():
+        return "<p>Hello, World!</p>"
+
+    return app
