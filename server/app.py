652d5a0852dda30cad3bbd77d416f2de0dd50275
ChuangWei Ma <chungwwei223@gmail.com>
2021-12-24 00:24:38 -0500

    api method to write stream sessions

--- server/app.py
+++ server/app.py
@@ -1,10 +1,10 @@
+from datetime import date, datetime
 from flask import Flask, request, jsonify
 from firebase_admin import credentials, firestore, initialize_app
 from math import floor
+import hull
 
-def create(test_source=None):
-    app = Flask(__name__)
-
+app = Flask(__name__)
 cred = credentials.Certificate('./fbkey.json')
 default_app = initialize_app(cred)
 db = firestore.client()
@@ -15,6 +15,48 @@ def determine_anchor_id(longitude, latitude):
     anchor_latitude = floor(latitude / SQUARE_SIZE) * SQUARE_SIZE
     anchor_longitude = floor(longitude / SQUARE_SIZE) * SQUARE_SIZE
     return (anchor_longitude, anchor_latitude)
+
+
+@app.route("/write_stream_session", methods=['POST'])
+def write_stream_session():
+    stream_session_ref = db.collection("stream-sessions")
+    try:
+        # extract fields
+        data = request.get_json()
+        userId = data['userId']
+        streamId = data['streamId']
+        videoCodec = data['videoCodec']
+        audioCodec = data['audioCodec']
+        videoResolution = data['videoResolution']
+        server = data['server']
+        duration = data['duration']
+        modemCount = data['modemCount']
+        unstableEvents = data['unstableEvents']
+
+        # POSIX/UNIX timestamp?
+        startDate = data['startDate']
+        endDate = data['endDate']
+        startDate = datetime.fromtimestamp(startDate)
+        endDate = datetime.fromtimestamp(endDate)
+
+        stream_session_ref.add({
+            'userId': userId,
+            'streamId': streamId,
+            'videoCodec': videoCodec,
+            'audioCodec': audioCodec,
+            'videoResolution': videoResolution,
+            'server': server,
+            'duration': duration,
+            'modemCount': modemCount,
+            'unstableEvents': unstableEvents,
+            'startDate': startDate,
+            'endDate': endDate,
+            'timestamp': firestore.SERVER_TIMESTAMP
+        })
+
+        return jsonify({"success": True}), 200
+    except Exception as e:
+        return f"An Error Occured: {e}"
 
 
 @firestore.transactional
@@ -81,7 +123,7 @@ def add_datapoint():
 def convert_to_point_geojson(data):
     points = []
     for b in data:
-        data = {
+        point = {
             "type": "Feature",
             "geometry": {
                 "type": "Point",
@@ -92,12 +134,27 @@ def convert_to_point_geojson(data):
                 "avgAudioBitrate": b['avgAudioBitrate']
             }
         }
-        points.append(data)
+        points.append(point)
     return {"type": "FeatureCollection", "features": points}
 
 
-def convert_to_polygon_geojson():
-    pass
+def convert_to_polygon_geojson(data, concave):
+    hull = map(list, concave)
+    polygons = []
+    for b in data:
+        polygon = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": [hull]
+            },
+            "properties": {
+                "avgBitrate": b['avgBitrate'],
+                "avgAudioBitrate": b['avgAudioBitrate']
+            }
+        }
+        polygons.append(polygon)
+    return {"type": "FeatureCollection", "features": polygons}
 
 
 def get_all_bins():
@@ -143,23 +200,38 @@ def get_all_bins():
             "avgBitrate": avg_bitrate,
             "avgAudioBitrate": avg_audio_bitrate
         })
-    # use convext hull to process each bin if we like to use polygon for the heatmap
-    # ######################################
 
-    # convert the process data into an proper geojson  
-    formatted = convert_to_point_geojson(preprocess)
-    return formatted
+    return preprocess
 
 
 @app.route('/point_bitrate_heatmap', methods=['GET'])
-def read():
+def point_heat_map():
+    try:
+        # get all bins
+        data = get_all_bins()
+
+        # convert the process data into an proper geojson  
+        formatted = convert_to_point_geojson(data)
+        return jsonify(formatted), 200
+    except Exception as e:
+        return f"An Error Occured: {e}"
+
+
+@app.route('/point_bitrate_heatmap', methods=['GET'])
+def polygon_heat_map():
     """
         read() : Fetches bin documents from Firestore collection as JSON.
     """
     try:
         # get all bins
         data = get_all_bins()
-        return jsonify(data), 200
+
+        points = data['points']
+        concave = hull.concave(points)
+
+        # convert the process data into an proper geojson  
+        formatted = convert_to_point_geojson(data, concave)
+        return jsonify(formatted), 200
     except Exception as e:
         return f"An Error Occured: {e}"
 
