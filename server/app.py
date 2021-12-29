dfc2cef32bf2b2993d52d50d7fd76b2093564237
ChuangWei Ma <39935368+chungwwei@users.noreply.github.com>
2021-12-28 21:39:11 -0500

    feat: api method to write stream sessions (#89)
    * api method to write stream sessions
    
    * fix: fix transaction bins and some typos

--- server/app.py
+++ server/app.py
@@ -1,15 +1,15 @@
+from datetime import date, datetime
 from flask import Flask, request, jsonify
 from firebase_admin import credentials, firestore, initialize_app
 from math import floor
+import hull
 
-def create(test_source=None):
-    app = Flask(__name__)
-
+app = Flask(__name__)
 cred = credentials.Certificate('./fbkey.json')
 default_app = initialize_app(cred)
 db = firestore.client()
 transaction = db.transaction()
-SQUARE_SIZE = 90
+SQUARE_SIZE = 0.002
 
 def determine_anchor_id(longitude, latitude):
     anchor_latitude = floor(latitude / SQUARE_SIZE) * SQUARE_SIZE
@@ -17,15 +17,71 @@ def determine_anchor_id(longitude, latitude):
     return (anchor_longitude, anchor_latitude)
 
 
+@app.route("/write_stream_session", methods=['POST'])
+def write_stream_session():
+    stream_session_ref = db.collection("stream-sessions")
+    try:
+        # extract fields
+        data = request.get_json()
+        userId = data['userId']
+        streamId = data['streamId']
+        videoCodec = data['videoCodec']
+        audioCodec = data['audioCodec']
+        videoResolution = data['videoResolution']
+        server = data['server']
+        duration = data['duration']
+        modemCount = data['modemCount']
+        unstableEvents = data['unstableEvents']
+
+        # POSIX/UNIX timestamp?
+        startDate = data['startDate']
+        endDate = data['endDate']
+        startDate = datetime.fromtimestamp(startDate)
+        endDate = datetime.fromtimestamp(endDate)
+
+        stream_session_ref.add({
+            'userId': userId,
+            'streamId': streamId,
+            'videoCodec': videoCodec,
+            'audioCodec': audioCodec,
+            'videoResolution': videoResolution,
+            'server': server,
+            'duration': duration,
+            'modemCount': modemCount,
+            'unstableEvents': unstableEvents,
+            'startDate': startDate,
+            'endDate': endDate,
+            'timestamp': firestore.SERVER_TIMESTAMP
+        })
+
+        return jsonify({"success": True}), 200
+    except Exception as e:
+        return f"An Error Occured: {e}"
+
+
 @firestore.transactional
-def update_bin_transaction(transaction, doc_ref, bitrate, audio_bitrate):
+def update_bin_transaction(transaction, doc_ref, anchor_lng, anchor_lat, bitrate, audio_bitrate, lng, lat):
     snapshot = doc_ref.get(transaction=transaction)
-    transaction.update(doc_ref, {
-        "totalBitrate": snapshot.get("totalBitrate") + bitrate,
-        "totalAudioBitrate": snapshot.get("totalAudioBitrate") + audio_bitrate,
-        "count": snapshot.get("count") + 1,
-        "timestamp": firestore.SERVER_TIMESTAMP
-    })
+    if snapshot.exists:
+        # update existing bin
+        transaction.update(doc_ref, {
+            "totalBitrate": snapshot.get("totalBitrate") + bitrate,
+            "totalAudioBitrate": snapshot.get("totalAudioBitrate") + audio_bitrate,
+            "count": snapshot.get("count") + 1,
+            "points": firestore.ArrayUnion([f"{lng},{lat}"]),
+            "timestamp": firestore.SERVER_TIMESTAMP
+        })
+    else:
+        # create a new bin
+        transaction.set(doc_ref, {
+            "anchorLongitude": anchor_lng,
+            "anchorLatitude": anchor_lat,
+            "totalBitrate": bitrate,
+            "totalAudioBitrate": audio_bitrate,
+            "count": 1,
+            "points": [f"{lng},{lat}"],
+            "timestamp": firestore.SERVER_TIMESTAMP
+        })
 
 
 @app.route("/write_datapoints", methods=['POST'])
@@ -43,7 +99,7 @@ def add_datapoints():
             anchor_longitude, anchor_latitude = determine_anchor_id(longitude, latitude)
             anchor_id = f"lng:{anchor_longitude},lat:{anchor_latitude}"
             doc_ref =  bins_ref.document(anchor_id)
-            update_bin_transaction(transaction, doc_ref, bitrate, audio_bitrate)
+            update_bin_transaction(transaction, doc_ref, anchor_longitude, anchor_latitude, bitrate, audio_bitrate, longitude, latitude)
         return jsonify({"success": True}), 200
 
     except Exception as e:
@@ -71,8 +127,7 @@ def add_datapoint():
 
         # pull the document from firebase with that anchor_id
         doc_ref = bin_ref.document(anchor_id)
-
-        update_bin_transaction(transaction, doc_ref, bitrate, audio_bitrate)
+        update_bin_transaction(transaction, doc_ref,  anchor_longitude, anchor_latitude, bitrate, audio_bitrate, longitude, latitude)
         return jsonify({"success": True}), 200
     except Exception as e:
         return f"An Error Occured: {e}"
@@ -81,7 +136,7 @@ def add_datapoint():
 def convert_to_point_geojson(data):
     points = []
     for b in data:
-        data = {
+        point = {
             "type": "Feature",
             "geometry": {
                 "type": "Point",
@@ -92,12 +147,28 @@ def convert_to_point_geojson(data):
                 "avgAudioBitrate": b['avgAudioBitrate']
             }
         }
-        points.append(data)
+        points.append(point)
     return {"type": "FeatureCollection", "features": points}
 
 
-def convert_to_polygon_geojson():
-    pass
+def convert_to_polygon_geojson(data):
+    polygons = []
+    for b in data:
+        points = b["points"]
+        shape = hull.convex(points)
+        polygon = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": [shape]
+            },
+            "properties": {
+                "avgBitrate": b['avgBitrate'],
+                "avgAudioBitrate": b['avgAudioBitrate']
+            }
+        }
+        polygons.append(polygon)
+    return {"type": "FeatureCollection", "features": polygons}
 
 
 def get_all_bins():
@@ -119,7 +190,6 @@ def get_all_bins():
     # preprocess all bins
     preprocess = []
     for b in bins:
-        _id = b['id']
         anchor_longitude = b['anchorLongitude']
         anchor_latitude = b['anchorLatitude']
         total_bitrate = b['totalBitrate']
@@ -141,25 +211,38 @@ def get_all_bins():
             "longitude": longitude,
             "latitude": latitude,
             "avgBitrate": avg_bitrate,
-            "avgAudioBitrate": avg_audio_bitrate
+            "avgAudioBitrate": avg_audio_bitrate,
+            "points": points
         })
-    # use convext hull to process each bin if we like to use polygon for the heatmap
-    # ######################################
 
-    # convert the process data into an proper geojson  
-    formatted = convert_to_point_geojson(preprocess)
-    return formatted
+    return preprocess
 
 
 @app.route('/point_bitrate_heatmap', methods=['GET'])
-def read():
+def point_heat_map():
+    try:
+        # get all bins
+        data = get_all_bins()
+
+        # convert the process data into an proper geojson  
+        formatted = convert_to_point_geojson(data)
+        return jsonify(formatted), 200
+    except Exception as e:
+        return f"An Error Occured: {e}"
+
+
+@app.route('/polygon_bitrate_heatmap', methods=['GET'])
+def polygon_heat_map():
     """
         read() : Fetches bin documents from Firestore collection as JSON.
     """
     try:
         # get all bins
         data = get_all_bins()
-        return jsonify(data), 200
+
+        # convert the process data into an proper geojson  
+        formatted = convert_to_polygon_geojson(data)
+        return jsonify(formatted), 200
     except Exception as e:
         return f"An Error Occured: {e}"
 
@@ -180,21 +263,21 @@ def write_anchors(anchors):
     return
 
 
-@app.route('/setbins', methods=['POST'])
-def setbins():
-    try:
-        anchors = set() 
-        for lng in range(-180, 180, 1):
-            for lat in range(-90, 90, 1):
-                a_tuple = determine_anchor_id(lng, lat)
-                anchors.add(a_tuple)
-        print(anchors)
-        print(f"there are {len(anchors)} anchors")
+# @app.route('/setbins', methods=['POST'])
+# def setbins():
+#     try:
+#         anchors = set() 
+#         for lng in range(-180, 180, 1):
+#             for lat in range(-90, 90, 1):
+#                 a_tuple = determine_anchor_id(lng, lat)
+#                 anchors.add(a_tuple)
+#         print(anchors)
+#         print(f"there are {len(anchors)} anchors")
 
-        # write the bins to database
-        write_anchors(anchors)
-        return "<p>Hello, World!</p>" 
+#         # write the bins to database
+#         write_anchors(anchors)
+#         return "<p>Hello, World!</p>" 
 
 
-    except Exception as e:
-        return f"An Error Occured: {e}"
+#     except Exception as e:
+#         return f"An Error Occured: {e}"
